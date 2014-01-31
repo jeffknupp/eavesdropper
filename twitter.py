b23a2dc4f0ad6406a2f79bdb19793d28beaf47d9
Jeff Knupp <jknupp@appnexus.com>
2014-01-31 09:47:15 -0500

    add dates

--- twitter.py
+++ twitter.py
@@ -16,29 +16,29 @@ CONSUMER_KEY = 'XCvzOpRBPjKoWFMSYnHqHg'
 CONSUMER_SECRET = 'flO2rrzu6xss6ubnJUS8hp0nhgbSYp8cjjCjnaHCzG8'
 client = AppClient(CONSUMER_KEY, CONSUMER_SECRET)
 access_token = client.get_access_token()
+QUERIES = ['jeffknupp.com', 'jeffknupp']
 
 def get_twitter_mentions():
-    response = client.api.search.tweets.get(q='jeffknupp.com', count=100)
-    statuses = response.data.statuses
+    statuses = []
+    for query in QUERIES:
+        response = client.api.search.tweets.get(q='jeffknupp.com', count=100)
+        statuses += response.data.statuses
     session = Session()
     twitter = session.query(Source).get(1)
-    with open('output.txt', 'w') as output:
-        output.write('Run ')
-        output.write(str(datetime.datetime.now))
-        output.write('\n')
-
-        new_mentions = 0
-        for status in statuses:
-            if not session.query(Mention).filter(Mention.domain_id==status.id_str).count():
-                m = Mention(text=status.text,
-                        associated_user='{} ({})'.format(status.user.screen_name,
-                            status.user.followers_count),
+    new_mentions = 0
+    for status in statuses:
+        if not session.query(Mention).filter(Mention.domain_id==status.id_str).count():
+            created_at = datetime.datetime.strptime(status.created_at, r"%a %b %d %H:%M:%S +0000 %Y")
+            m = Mention(text=status.text,
+                    associated_user='{} ({})'.format(status.user.screen_name,
+                        status.user.followers_count),
+                        recorded_at=datetime.datetime.now(),
+                        occurred_at=created_at,
                         source=twitter,
                         domain_id=status.id_str)
-                output.write('added ')
-                new_mentions += 1
-                session.add(m)
-        session.commit()
+            new_mentions += 1
+            session.add(m)
+    session.commit()
     return new_mentions
 
 if __name__ == '__main__':
